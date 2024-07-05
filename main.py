352e106621d346b8f2d967f394f8de175481f3be
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2024-07-05 18:19:24 +0600

    completed

--- main.py
+++ main.py
@@ -65,17 +65,77 @@ algo expert problems for the coding test.
 #         else:
 #             nums[num] = True
 #     return []
+#
+# def two_number_sum(array, target_sum):
+#     array.sort()
+#     left = 0
+#     right = len(array) - 1
+#     while left < right:
+#         current_sum = array[left] + array[right]
+#         if current_sum == target_sum:
+#             return [array[left], array[right]]
+#         elif current_sum < target_sum:
+#             left += 1
+#         elif current_sum > target_sum:
+#             right -= 1
+import math
+import sys
 
-def two_number_sum(array, target_sum):
-    array.sort()
-    left = 0
-    right = len(array) - 1
-    while left < right:
-        current_sum = array[left] + array[right]
-        if current_sum == target_sum:
-            return [array[left], array[right]]
-        elif current_sum < target_sum:
-            left += 1
-        elif current_sum > target_sum:
-            right -= 1
+# arr = []
+odd = 0
+even = 0
+input = sys.stdin.read
+arr = data = sys.stdin.read().strip().split('\n')
 
+# for i in data:
+#     try:
+#         a = input()
+#     except EOFError:
+#         break
+#     else:
+#         arr.append(a)
+#         print(arr)
+# print(arr)
+max_len = 0
+for i in arr:
+    if len(i) > max_len:
+        max_len = len(i)
+        # print("hlo")
+
+    else:
+        pass
+#         print("hi")
+print("*" * (max_len + 2))
+for i in arr:
+    if len(i) <= max_len:
+        if (max_len % 2 == 0 and len(i) % 2 == 0) or (max_len % 2 == 1 and len(i) % 2 == 1):
+            g = math.floor((max_len - len(i))/2)
+            space = " " * (g)
+            i = "*" + space + i + space + "*"
+            print(i)
+        elif max_len % 2 == 0 and len(i) % 2 == 1:
+            g = math.ceil((max_len - len(i)) / 2)
+            space = " " * (g)
+            if odd % 2 == 0:
+                odd += 1
+                i = "*" + " " * (g - 1) + i + space + "*"
+                print(i)
+            if odd % 2 == 1:
+                odd += 1
+                i = "*" + space + i + " " * (g - 1) + "*"
+        elif max_len % 2 == 1 and len(i) % 2 == 0:
+            g = math.floor((max_len - len(i)) / 2)
+            space = " " * (g)
+            if even % 2 == 0:
+                even += 1
+                i = "*" + " " * (g - 1) + i + space + "*"
+                print(i)
+            if even % 2 == 1:
+                even += 1
+                i = "*" + space + i + " " * (g - 1) + "*"
+                print(i)
+
+print("*" * (max_len + 2))
+
+# print(arr)
+# print("*" * (max_len + 2))
