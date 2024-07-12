3f265ea32af4edc4586842d029424051c4049d12
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2024-07-12 13:17:54 +0600

    complete contest 5

---
+++ div3.py
@@ -0,0 +1,16 @@
+# A
+# t = int(input())
+# while t > 0:
+#     arr = list(map(int, input().split()))
+#     brr = sorted(arr)
+#     mx = 0
+#     for i in range(5):
+#         brr[0] += 1
+#         brr = sorted(brr)
+#     print(brr[0] * brr[1] * brr[2])
+#     t -= 1
+
+
+
+
+
